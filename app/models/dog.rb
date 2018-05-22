b5c9f548e788c8fd6388ea3e9c494dece839ba35
Alyssa Kim <alyssa.kim@gmail.com>
2018-05-22 10:22:58 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,15 +1,17 @@
-class Dog 
-  attr_accessor :name, :breed, :age 
+class Dog
+  attr_accessor :name, :breed, :age
 
   @@all = []
-  
-  def initialize 
-    @name = name 
+
+  def initialize
+    @name = name
     @breed = breed
     @age = age
     @@all << self 
-  end 
+  end
 
-  def self.all 
-  end 
-end 
+  def self.all
+    @@all
+  end
+  
+end
